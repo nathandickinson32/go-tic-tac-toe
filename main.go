6d1ce2fa6673d2a6ca4cc03dd6bcba4107c0251e
nathandickinson32 <nathandickinson32@gmail.com>
2026-01-13 17:05:19 -0500

    included new testing style to mimic describe/context, new techniques like errorF and FatalF, added getAvailableMoves

--- main.go
+++ main.go
@@ -8,13 +8,27 @@ func initBoard() Board {
 	var board Board
 	num := 1
 
-	for i := range 3 {
-		for j := range 3 {
-			board[i][j] = fmt.Sprintf("%d", num)
+	for row := range 3 {
+		for col := range 3 {
+			board[row][col] = fmt.Sprintf("%d", num)
 			num++
 		}
 	}
 	return board
+}
+
+func getAvailableMoves(board Board) []int {
+	var moves []int
+	num := 1
+	for i := range 3 {
+		for j := range 3 {
+			if board[i][j] != "X" && board[i][j] != "O" {
+				moves = append(moves, num)
+			}
+			num = num + 1
+		}
+	}
+	return moves
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,43 +1,94 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestBoard(t *testing.T) {
-	board := initBoard()
-	if board[0][0] != "1" {
-		t.Error("Expected board[0][0] to be 1, got:", board[0][0])
-	}
+func TestBoardConditions(t *testing.T) {
 
-	if board[0][1] != "2" {
-		t.Error("Expected board[0][1] to be 2, got:", board[0][1])
-	}
+	t.Run("getting all positions", func(t *testing.T) {
 
-	if board[0][2] != "3" {
-		t.Error("Expected board[0][2] to be 3, got:", board[0][2])
-	}
+		t.Run("gets all 9 board positions for 3x3 board", func(t *testing.T) {
+			board := initBoard()
 
-	if board[1][0] != "4" {
-		t.Error("Expected board[1][0] to be 4, got:", board[1][0])
-	}
-
-	if board[2][2] != "9" {
-		t.Error("Expected board[2][2] to be 9, got:", board[2][2])
-	}
-}
-
-func TestInitBoard(t *testing.T) {
-	board := initBoard()
-
-	expected := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	position := 0
-
-	for i := range 3 {
-		for j := range 3 {
-			if board[i][j] != expected[position] {
-				t.Errorf("Expected board[%d][%d] to be '%s', got '%s'",
-					i, j, expected[position], board[i][j])
+			expected := [3][3]string{
+				{"1", "2", "3"},
+				{"4", "5", "6"},
+				{"7", "8", "9"},
 			}
-			position++
-		}
-	}
+
+			for i := range 3 {
+				for j := range 3 {
+					if board[i][j] != expected[i][j] {
+						t.Errorf("Expected board[%d][%d] to be '%s', got '%s'",
+							i, j, expected[i][j], board[i][j])
+					}
+				}
+			}
+		})
+	})
+
+	t.Run("getting available 3x3 moves", func(t *testing.T) {
+
+		t.Run("gets one available move 3x3", func(t *testing.T) {
+			testBoard := Board{
+				{"O", "X", "O"},
+				{"X", "X", "O"},
+				{"X", "O", "9"},
+			}
+
+			moves := getAvailableMoves(testBoard)
+
+			if len(moves) != 1 {
+				t.Fatalf("Expected 1 available move, got %d", len(moves))
+			}
+
+			if moves[0] != 9 {
+				t.Errorf("Expected move 9, got %d", moves[0])
+			}
+		})
+
+		t.Run("gets all available moves for 3x3", func(t *testing.T) {
+			testBoard := Board{
+				{"O", "X", "O"},
+				{"4", "X", "6"},
+				{"X", "O", "9"},
+			}
+
+			moves := getAvailableMoves(testBoard)
+
+			if len(moves) != 3 {
+				t.Fatalf("Expected 3 available moves, got %d", len(moves))
+			}
+
+			expectedMoves := []int{4, 6, 9}
+			for _, expected := range expectedMoves {
+				found := slices.Contains(moves, expected)
+				if !found {
+					t.Errorf("Expected move %d to be available", expected)
+				}
+			}
+		})
+
+		t.Run("does not contain moves that are taken for 3x3", func(t *testing.T) {
+			testBoard := Board{
+				{"O", "X", "O"},
+				{"4", "X", "6"},
+				{"X", "O", "9"},
+			}
+
+			moves := getAvailableMoves(testBoard)
+
+			takenPositions := []int{1, 2, 3, 5, 7, 8}
+			for _, taken := range takenPositions {
+				for _, move := range moves {
+					if move == taken {
+						t.Errorf("Position %d should not be available (it's taken)", taken)
+					}
+				}
+			}
+		})
+	})
+
 }
